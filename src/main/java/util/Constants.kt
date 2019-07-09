6811b54c6e7df995c3c1588ebc23bf7b4ad34112
Logan Darklock <logandarklock@gmail.com>
2019-07-09 02:08:06 -0700

    Version... unbump. First version that'll have a release

--- src/main/java/util/Constants.kt
+++ src/main/java/util/Constants.kt
@@ -4,6 +4,6 @@ import org.apache.logging.log4j.Logger
 
 const val modid = "diamond2potato"
 const val name = "Diamond to Potato"
-const val version = "1.0"
+const val version = "0.1"
 
 lateinit var logger: Logger
